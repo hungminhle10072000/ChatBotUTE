05c710b7d7c1d7854897b6635fc349b5bb874182
hungduong.mess32@gmail.com <minhle10072000>
2021-09-16 17:14:42 +0700

    ChatBotUTE

---
+++ Nhom2_ChatBotUTE/actions/actions.py
@@ -0,0 +1,50 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+# from typing import Any, Text, Dict, List
+#
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import requests
+
+# class ActionHelloLoc(Action):
+#
+#     def name(self) -> Text:
+#         return "action_get_loc"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#         slot_name = tracker.get_slot("state")
+#
+#         print("slotname", slot_name)
+#
+#         dispatcher.utter_message(
+#             text="So You Live In " + slot_name.title() + " , Here Are Your Location's Corona Stats: \n")
+#
+#         return []
